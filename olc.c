f700cf921a5aa682587a1ee15bc76b314fd53795
MichaelMyers0 <siracusa.augusta@gmail.com>
2025-04-28 12:34:39 +0300

    nineth commit

--- olc.c
+++ olc.c
@@ -35,7 +35,19 @@ char* msg;
 main()
 {
 	int fd;
+	struct sockaddr_in6 s;
+	const struct in6_addr in6addr_any = IN6ADDR_ANY_INIT;
+	socklen_t slen;
+
+	s.sin6_family = AF_INET6;
+	s.sin6_port = htons(olc_client1_port);
+	s.sin6_addr = in6addr_any;
+	slen = sizeof(s);
+
 	create_socket(&fd, AF_INET6, SOCK_STREAM);
+	bind_socket(fd, (const struct sockaddr*)&s, slen);
+	reuse_port(fd);
 	close_socket(fd);
+
 	_exit(0);
 }
